3a9c919b58ed9d6f5646bf4d5803363fa12c67be
Manfred Weber <manfred.weber@gmail.com>
2014-12-28 20:36:25 +0000

    remove mean

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
+  res.send('<h1>Hello world</h1>');
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });

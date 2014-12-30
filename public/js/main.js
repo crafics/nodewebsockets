b347489a956178cb0937221258e0410a001443c2
Manfred Weber <manfred.weber@gmail.com>
2014-12-30 14:27:38 +0000

    some testing

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,8 @@ $(document).ready(function() {
         DEU: { fillKey: "authorHasTraveledTo" },
       }
     });
-    
-    var colors = d3.scale.category10();
     /*
+    var colors = d3.scale.category10();
     window.setInterval(function() {
       basic_choropleth.updateChoropleth({
         USA: colors(Math.random() * 10),
@@ -30,4 +29,40 @@ $(document).ready(function() {
       });
     }, 2000);
     */
+    basic_choropleth.arc([
+      {
+          origin: {
+              latitude: 40.639722,
+              longitude: -73.778889
+          },
+          destination: {
+              latitude: 37.618889,
+              longitude: -122.375
+          }
+      },
+      {
+          origin: {
+              latitude: 30.194444,
+              longitude: -97.67
+          },
+          destination: {
+              latitude: 25.793333,
+              longitude: -80.290556
+          },
+          options: {
+            strokeWidth: 2,
+            strokeColor: 'rgba(100, 10, 200, 0.4)'
+          }
+      },
+      {
+          origin: {
+              latitude: 39.861667,
+              longitude: -104.673056
+          },
+          destination: {
+              latitude: 35.877778,
+              longitude: -78.7875
+          }
+      }
+    ],  {strokeWidth: 1, arcSharpness: 1.4});
 });
